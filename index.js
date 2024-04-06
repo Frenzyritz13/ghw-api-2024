05248f8cb5d680914d896e6f89967d77c2e4276c
Ritvi Mishra <ritvi12@gmail.com>
2024-04-06 19:25:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,29 +3,35 @@ const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const app = express()
+
 const challenges = []
 
-app.get('/', async (req, res) => {res.json({message: 'Hello GHW! Welcome to our stream!'})})
-
-app.get('/challenges', async (req, res) => {
-
-    axios.get('https://ghw.mlh.io/challenges')
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-
-        $('a.contains("")',html).each(function(){
-            const title = $(this).text()
-            const url = $(this).attr('href')
-
-            challenges.push({
-                title,
-                url
-            })
-        }).catch(err => console.log(err))
-
-        res.json(challenges)
-    })
+app.get('/', (req, res) => {
+    res.json('Welcome to GHW Stream demo')
 })
 
-app.listen(PORT, () =>console.log(`Server running on PORT ${PORT}`))
+app.get('/challenges', (req, res) => {
+
+
+
+    axios.get('https://ghw.mlh.io/challenges')
+            .then(response => {
+                const html = response.data
+                const $ = cheerio.load(html)
+                
+    
+                $('a:contains("")', html).each(function () {
+                    const title = $(this).text()
+                    const url = $(this).attr('href')
+                    challenges.push({
+                        title,
+                        url
+                    })        
+                })
+                res.json(challenges)
+            }).catch(err => console.log(err))
+    })
+    
+    
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
